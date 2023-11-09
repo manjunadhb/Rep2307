65e4520e324e7cb590c5a0e228a5c34b86feef69
Jyothi-Gaddameedi <g.jyothi.779@gmail.com>
2023-11-09 16:41:18 +0530

    added dhoni details

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
   return (
     <div className="App">
       <h1>This line is added by Manjunadh</h1>
+      <h1>About Dhoni</h1>
+      <img src="./images/Dhoni.jpg"></img>
+      <h2>Born:7 July 1981</h2>
+      <h2>Parents: Pan Singh Dhoni, Devaki Devi</h2>
+      <h2>Spouse:Sakshi Dhoni</h2>
     </div>
   );
 }
