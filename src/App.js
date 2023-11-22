75d9a24a35812e66341f51ebc92318d0c89b5163
Kalyani <kalyaniambhore98@gmail.com>
2023-11-22 16:41:31 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <h2>Spouse:Sakshi Dhoni</h2> 
       <h1>Hello All,Welcome to GIT HUB</h1>  
       <img src="./images/git hub"></img>
-      <h1>FullStck Development By using Javascript Languege</h1>
+      <h1>FullStck Development By using Javascript </h1>
       <img src="./images/javascript.jpeg"></img>
     </div>
   );
